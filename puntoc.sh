ada77bad8c74834aafe2f2df4e82a17f8418020d
vitaccajulian <vitaccajulian@gmail.com>
2024-10-18 00:18:26 +0000

    subo punto c terminado y validar en carpetas de alumnos y profesores

--- puntoc.sh
+++ puntoc.sh
@@ -13,3 +13,15 @@ sudo usermod -aG grupodevops developer1
 sudo usermod -aG grupodevops devops1
 sudo usermod -aG grupodevops devops2
 sudo usermod -aG grupotesters tester1
+
+sudo chmod -R 750 Examenes-UTN/alumno_1
+sudo chmod -R 760 Examenes-UTN/alumno_2 
+sudo chmod -R 700 Examenes-UTN/alumno_3
+sudo chmod -R 775 Examenes-UTN/profesores
+
+
+whoami > Examenes-UTN/alumno_1/validar.txt
+whoami > Examenes-UTN/alumno_2/validar.txt
+whoami > Examenes-UTN/alumno_3/validar.txt
+whoami > Examenes-UTN/profesores/validar.txt
+
